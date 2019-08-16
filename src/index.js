cc2c07a340ed9f44eb627e022cf7cd8e477a0b16
wangqiaoqiaogithub <wangqiaot1128@qq.com>
2019-08-16 23:24:28 +0800

    update to ScrollToTop

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import {ScrollToTop} from './js/scrollToTop.js';
 let scrollTotop = require('./js/scrollToTop.js');
 require('./css/common.css');
 console.log(scrollTotop+1);
-var stp = new ScrollToTop('#scrolltop',{
+var stp = new ScrollToTop('#stcontainer',{
     speed: 10,//点击时滚动条向上回去时过渡的速度
     scrolldistance: 40//垂直滚动条的距离
 })
